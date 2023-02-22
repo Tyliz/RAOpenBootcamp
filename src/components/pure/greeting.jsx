f583f85d73a35f7bb5599729ea8165f698f39420
TylizRv <caosavenger@gmail.com>
2023-02-22 06:25:15 -0400

    Clase 16: Rutas

--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 
 class Greeting extends Component {
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            age : 26
-        }
-    }
+	constructor (props) {
+		super(props)
+		this.state = {
+			age : 26
+		}
+	}
 
-    render() {
-        return (
-            <div>
-                <h1>Hi { this.props.name }!</h1>
-                <h2>You are { this.state.age } years old.</h2>
-                <div>
-                    <button onClick={this.birthday}>
-                        <span>Birthday!</span>
-                    </button>
-                </div>
-            </div>
-        );
-    }
+	birthday() {
+		this.setState((prevState) => (
+			{
+				age: prevState.age + 1
+			}
+		))
+	}
 
-    birthday = () => {
-        this.setState((prevState) => (
-            {
-                age: prevState.age + 1
-            }
-        ))
-    }
+	render() {
+		return (
+			<div>
+				<h1>Hi { this.props.name }!</h1>
+				<h2>You are { this.state.age } years old.</h2>
+				<div>
+					<button onClick={this.birthday}>
+						<span>Birthday!</span>
+					</button>
+				</div>
+			</div>
+		)
+	}
 }
 
 // Content of component that can be pass by a parent component
 Greeting.propTypes = {
-    name: PropTypes.string,
-};
+	name: PropTypes.string,
+}
 
 
-export default Greeting;
+export default Greeting
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { useNavigate  } from 'react-router-dom'
 
 const HomePage = () => {
+	const navigate = useNavigate()
+
+	const goProfile = () => {
+		navigate('/profile')
+	}
+
+	const goBack = () => {
+		navigate(-1)
+	}
+
+	const goForward = () => {
+		navigate(+1)
+	}
+
 	return (
 		<div>
 			<h1>Home Page</h1>
-			{/* TODO: Create a Dashborad */}
-			<h2>Access to your Task</h2>
+			<button onClick={ goProfile }>
+				Go to Profile
+			</button>
+			<button onClick={ goBack }>
+				Go Back
+			</button>
+			<button onClick={ goForward }>
+				Go Forward
+			</button>
 		</div>
-	);
-};
+	)
+}
 
-export default HomePage;
+export default HomePage
