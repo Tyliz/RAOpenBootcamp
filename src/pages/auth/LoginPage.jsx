b90a49da64efb4cf275fc3719737f0a6ac488909
TylizRv <caosavenger@gmail.com>
2023-02-22 20:45:08 -0400

    Clase Rutas

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from '../../components/pure/forms/loginForm'
 
 const LoginPage = () => {
 	return (
 		<div>
-			<h1>Login Page</h1>
+			<LoginForm />
 		</div>
 	)
 }
