68eb27f51d83212895913bf31ceefbae21af15eb
TylizRv <caosavenger@gmail.com>
2023-03-02 20:01:10 -0400

    Clase de asincronos y Observables

---
+++ src/services/observableService.js
@@ -0,0 +1,14 @@
+import {
+	Observable,
+} from 'rxjs'
+
+export const getNumbers = new Observable(suscriber => {
+	// * We emit values
+	suscriber.next(1) // Emits 1
+	suscriber.next(2) // Emits 2
+	suscriber.next(3) // Emits 3
+	setTimeout(() => {
+		suscriber.next(4) // Emits 4
+		suscriber.complete() // Finally, the Observable completes & finishes
+	}, 1000) // Waits 1s
+})
